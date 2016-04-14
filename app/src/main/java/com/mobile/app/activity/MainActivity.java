4fde25539d947d22960ec3e8e4d38725148636f3
lei_qing <leiqing010@gmail.com>
2016-04-14 14:43:38 +0200

    update userInfo

--- app/src/main/java/com/mobile/app/activity/MainActivity.java
+++ app/src/main/java/com/mobile/app/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.mobile.app.activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,24 +17,39 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.mobile.app.R;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-   //bottom buttons
-    private  ImageView[] imageButtons;
+    //bottom buttons
+    private ImageView[] imageButtons;
 //    private RelativeLayout bot_menu;
 //    private RelativeLayout bot_restaurant;
 //    private RelativeLayout bot_order;
 //    private RelativeLayout bot_user;
 
+    //unread orders number
+    private TextView unreadOrder;
+
     //Fragment manager
     private FragmentManager fragmentManager;
 
     private Fragment[] fragments;
+    private FragmentMenu fragmentMenu;
+    // private FragmentRestaurant
+    private FragmentProfile fragmentProfile;
+
     private TextView[] textViews;
     private int index;
     private int currentTabIndex;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -54,14 +70,27 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 //                        .setAction("Action", null).show();
 //            }
 //        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initViews() {
+
+
+        unreadOrder = (TextView) findViewById(R.id.unread_orders);
+
+        fragmentMenu = new FragmentMenu();
+        fragmentProfile = new FragmentProfile();
+        fragments = new Fragment[] {fragmentMenu,fragmentProfile };
+
         imageButtons = new ImageView[4];
-        imageButtons[0] =(ImageView) findViewById(R.id.img_menu);
+        imageButtons[0] = (ImageView) findViewById(R.id.img_menu);
         imageButtons[1] = (ImageView) findViewById(R.id.img_restaurant);
         imageButtons[2] = (ImageView) findViewById(R.id.img_order);
         imageButtons[3] = (ImageView) findViewById(R.id.img_order);
+
+        imageButtons[0].setSelected(true);
 
 //        imageButtons[0].setOnClickListener(this);
 //        imageButtons[1].setOnClickListener(this);
@@ -69,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 //        imageButtons[3].setOnClickListener(this);
 
         //default fragment(menu)
-        imageButtons[0].setSelected(true);
+
 
         textViews = new TextView[4];
         textViews[0] = (TextView) findViewById(R.id.text_menu);
@@ -77,13 +106,20 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         textViews[2] = (TextView) findViewById(R.id.text_order);
         textViews[3] = (TextView) findViewById(R.id.text_user);
         textViews[0].setTextColor(0xFF45C01A);
+        getFragmentManager().beginTransaction()
+//                .add(R.id.fragment_container,fragmentMenu)
+//                .add(R.id.fragment_container,fragmentProfile)
+//                .add(R.id.fragment_container,fragmentProfile)
+                .add(R.id.fragment_container,fragmentProfile)
+//                .hide(fragmentProfile).hide(fragmentProfile)
+                .hide(fragmentProfile).show(fragmentProfile).commit();
 
 
     }
 
     @Override
     public void onClick(View v) {
-        switch ((v.getId())){
+        switch ((v.getId())) {
             case R.id.bot_menu:
                 index = 0;
                 break;
@@ -100,15 +136,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                 break;
         }
 
-        if(currentTabIndex != index){
+        if (currentTabIndex != index) {
             FragmentTransaction transaction = getFragmentManager()
                     .beginTransaction();
             transaction.hide(fragments[currentTabIndex]);
-            if(!fragments[index].isAdded()){
-                transaction.add(R.id.fragment_container,fragments[index]);
-            }
-            else
-            transaction.show(fragments[index]).commit();
+            if (!fragments[index].isAdded()) {
+                transaction.add(R.id.fragment_container, fragments[index]);
+            } else
+                transaction.show(fragments[index]).commit();
         }
         imageButtons[currentTabIndex].setSelected(false);
         imageButtons[index].setSelected(true);
@@ -119,33 +154,4 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
 
 
-
-
-
-
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
